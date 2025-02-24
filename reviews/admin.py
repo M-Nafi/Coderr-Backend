eb98a9db96db36773209364732b2971287ff6b51
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-02-24 08:30:25 +0100

    created rewievs models according to requirements. created view for the admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('business_user', 'reviewer', 'rating', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('business_user__username', 'reviewer__username')
+
+admin.site.register(Review, ReviewAdmin)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from offers.models import Offer
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    reviewer = models.ForeignKey(User, related_name='reviewer', on_delete=models.CASCADE)
+    business_user = models.ForeignKey(User, related_name='business_reviewer', on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.reviewer} business_reviewer {self.business_user}"
+    
+    def update(self, *args, **kwargs):
+        self.updated_at = now()
+        super().save(*args, **kwargs)
