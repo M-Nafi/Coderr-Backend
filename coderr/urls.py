3123d4edbff08df384c0447bb2b9df9d23c33386
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-03-04 12:13:28 +0100

    added reviews url in coderr app and completed views in reviews. everything still needs to be tested

--- coderr/urls.py
+++ coderr/urls.py
@@ -19,11 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user_auth.api.urls')),
     path('api/', include('base_info.api.urls')),
     path('api/', include('offers.api.urls')),
     path('api/', include('orders.api.urls')),
+    path('api/', include('reviews.api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- reviews/api/views.py
+++ reviews/api/views.py
@@ -5,13 +5,14 @@ from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
 
 class ReviewListAPIView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     ordering_fields = ['updated_at', 'rating']
     filterset_fields = ['business_user_id', 'reviewer_id']
-    filter_backends = [filters.Orderingfilter, DjangoFilterBackend]
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
     permission_classes = [permissions.AllowAny]
 
     def get_permissions(self):
@@ -23,3 +24,33 @@ class ReviewListAPIView(generics.ListCreateAPIView):
         if not self.request.user.profile.type == 'customer':
             raise PermissionDenied("Nur Kunden haben Zugriff auf diese Funktion.")
         serializer.save(reviewer=self.request.user)
+
+class ReviewDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        if serializer.instance.reviewer != self.request.user and not self.request.user.is_staff:
+            raise PermissionDenied("Nur der Verfasser oder ein Admin darf diese Bewertung bearbeiten.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.reviewer != self.request.user and not self.request.user.is_staff:
+            raise PermissionDenied("Nur der Verfasser oder ein Admin darf diese Bewertung löschen.")
+        instance.delete()
+
+    
